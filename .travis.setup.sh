410e5ff6da31b4306f975da6c394a7a96f72f6a6
Gleb Mazovetskiy <glex.spb@gmail.com>
2016-12-01 20:12:56 +0000

    f

--- .travis.setup.sh
+++ .travis.setup.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 if [ "$DB" = 'mysql2' ]; then
-  mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY ''"
+  mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password"
 fi
