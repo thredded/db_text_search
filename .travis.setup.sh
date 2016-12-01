612fd1ec028291b6b73f9dd137777f3cd0d493de
Gleb Mazovetskiy <glex.spb@gmail.com>
2016-12-01 20:09:35 +0000

    user2?

--- .travis.setup.sh
+++ .travis.setup.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 if [ "$DB" = 'mysql2' ]; then
-  mysql -u root -e "CREATE USER 'travis'@'127.0.0.1' IDENTIFIED WITH mysql_native_password BY ''"
-  mysql -u root -e "GRANT ALL ON travis.* TO 'travis'@'%';"
+  mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY ''"
 fi
