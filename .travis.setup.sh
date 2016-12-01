d68cfe4d4aebce51c8991a0910305f141105b0b0
Gleb Mazovetskiy <glex.spb@gmail.com>
2016-12-01 20:24:19 +0000

    Travis (#3)
    Update travis config to work with the new build env update:
    https://docs.travis-ci.com/user/build-environment-updates/2016-12-01/

--- .travis.setup.sh
+++ .travis.setup.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
-
-# TODO: remove this once travis gets on with times and upgrades MySQL to 5.6.4+
 if [ "$DB" = 'mysql2' ]; then
-  sudo -E apt-get -yq update &>> ~/apt-get-update.log
-  sudo -E apt-get -yq --no-install-suggests --no-install-recommends --force-yes install \
-    mysql-server-5.6 mysql-client-core-5.6 mysql-client-5.6
+  # work around https://github.com/travis-ci/travis-ci/issues/6961
+  mysql -u root -e "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('')"
 fi
